9ffc2f8257d22d0d87b4526814a4b152518a14e5
alstreid <alexsonder01@gmail.com>
2023-12-05 22:37:29 -0500

    correcion-despliegeSideBar

--- src/app/side.tsx
+++ src/app/side.tsx
@@ -10,10 +10,12 @@ const Sidebar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <div className={`${toggle ? "w-[5.8rem]" : ""}
-    sidebar-container`}>
+    sidebar-container`} onClick={() => {
+      setToggle(!toggle);
+    }} >
       <UserPerfil toggle={toggle}/>
       <SideBarData toggle={toggle}/>
-      <div 
+      {/* <div 
       className="absolute top-[7rem] flex 
       justify-center items-center -left-5 w-10 h-10 
       bg-gray-300 rounded-full cursor-pointer shadow-md"
@@ -24,7 +26,7 @@ const Sidebar = () => {
         className={`${
           toggle ? "rotate-180" : ""
           } text-3xl transition-all duration-300`}/>
-      </div>
+      </div> */}
       </div>
   )
 }
