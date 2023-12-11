6f9fec549351041cfa3819e4c4b1b1f3e65e8b0c
alstreid <alexsonder01@gmail.com>
2023-12-10 22:46:34 -0500

    Modificacion-Modals-ALEX

--- src/componentes/IngresosBGmodal.tsx
+++ src/componentes/IngresosBGmodal.tsx
@@ -1,131 +1,118 @@
 import { useState } from 'react';
 
-const Modal = ({ show, handleClose, handleAccept }) => {
+const Modal = ({ show, handleClose }) => {
+  const [value, setValue] = useState('');
+  const [movementType, setMovementType] = useState('');
+  const [cardType, setCardType] = useState('');
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [value, setValue] = useState('');
-  const [movementType, setMovementType] = useState(''); // Gasto, Ingreso, Ahorro
-  const [selectedCard, setSelectedCard] = useState(''); // Tarjeta seleccionada
-  const [showCardSelect, setShowCardSelect] = useState(false); // Controlar visibilidad del segundo select
 
-  const handleInputChange = (e, field) => {
-    const value = e.target.value;
-    switch (field) {
-      case 'name':
-        setName(value);
-        break;
-      case 'description':
-        setDescription(value);
-        break;
-      case 'value':
-        setValue(value);
-        break;
-      default:
-        break;
+  const handleAcceptClick = () => {
+    if (value && movementType && ((movementType === 'Ahorro' && cardType) || (['Gasto', 'Ingreso'].includes(movementType) && name && description))) {
+      const data = {
+        value,
+        movementType,
+        cardType,
+        name,
+        description,
+      };
+      console.log(data);
+      handleClose();
+      handleResetFields();
+    } else {
+      alert('Por favor complete todos los campos.');
     }
   };
+
   const handleResetFields = () => {
-    setName('');
-    setDescription('');
     setValue('');
     setMovementType('');
-    setSelectedCard('');
-    setShowCardSelect(false);
-  };
-
-  const handleMovementTypeChange = (e) => {
-    const selectedType = e.target.value;
-    setMovementType(selectedType);
-    if (selectedType === 'Ahorro') {
-      // Si se selecciona 'Ahorro', mostrar el segundo select
-      setShowCardSelect(true);
-    } else {
-      // Si no es 'Ahorro', ocultar el segundo select
-      setShowCardSelect(false);
-      setSelectedCard('');
-    }
-  };
-
-  const handleSelectedCardChange = (e) => {
-    const selectedCard = e.target.value;
-    setSelectedCard(selectedCard);
-  };
-  
-  
-  const handleAcceptClick = () => {
-    // Aquí se puede realizar la acción con los datos ingresados
-    const data = {
-      name,
-      description,
-      value,
-      movementType,
-      selectedCard, // Aquí puedes utilizar la tarjeta seleccionada si está disponible
-    };
-    handleAccept(data);
-    handleResetFields();
-    console.log(data);
+    setCardType('');
+    setName('');
+    setDescription('');
   };
 
   return (
     <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center ${show ? 'block' : 'hidden'}`}>
-      <div className="bg-white p-6 rounded-lg w-2/6 shadow-md">
-        <h2 className="text-lg font-bold mb-4">Ingresar Movimiento</h2>
-        <input
-          type="text"
-          placeholder="Nombre"
-          value={name}
-          onChange={(e) => handleInputChange(e, 'name')}
-          className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
-        />
-        <input
-          type="text"
-          placeholder="Descripción"
-          value={description}
-          onChange={(e) => handleInputChange(e, 'description')}
-          className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
-        />
+      <div className="bg-white p-6 rounded-lg shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-4 text-center">Ingresar Movimiento</h2>
         <input
           type="number"
           placeholder="Valor"
           value={value}
-          onChange={(e) => handleInputChange(e, 'value')}
+          onChange={(e) => setValue(e.target.value)}
           className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+          required
         />
         <select
           value={movementType}
-          onChange={handleMovementTypeChange}
+          onChange={(e) => {
+            setMovementType(e.target.value);
+            setCardType('');
+            setName('');
+            setDescription('');
+          }}
           className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+          required
         >
           <option value="">Seleccionar tipo de movimiento</option>
           <option value="Ahorro">Ahorro</option>
           <option value="Gasto">Gasto</option>
           <option value="Ingreso">Ingreso</option>
         </select>
-        {showCardSelect && (
+
+        {movementType === 'Ahorro' && (
           <select
-            value={selectedCard}
-            onChange={handleSelectedCardChange}
+            value={cardType}
+            onChange={(e) => {
+              setCardType(e.target.value);
+              setName('');
+              setDescription('');
+            }}
             className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+            required
           >
             <option value="">Seleccionar tarjeta</option>
-            {/* Aquí puedes mapear las opciones de nombres de tarjetas */}
-            <option value="Tarjeta 1">Tarjeta 1</option>
-            <option value="Tarjeta 2">Tarjeta 2</option>
-            {/* ... Otras opciones de nombres de tarjetas */}
+            <option value="card-1">Card-1</option>
+            <option value="card-2">Card-2</option>
+            <option value="card-3">Card-3</option>
           </select>
         )}
+
+        {['Gasto', 'Ingreso'].includes(movementType) && (
+          <>
+            <input
+              type="text"
+              placeholder="Nombre"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+              required
+            />
+            <input
+              type="text"
+              placeholder="Descripción"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 mb-4 w-full"
+              required
+            />
+          </>
+        )}
+
         <div className="flex justify-center">
-          <button onClick={handleClose} className="bg-neutral-700 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2">
+          <button onClick={() => { handleClose(); handleResetFields(); }} className="bg-neutral-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2">
             Cancelar
           </button>
-          <button onClick={handleAcceptClick} className="bg-neutral-700 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Aceptar
+          <button onClick={handleAcceptClick} className="bg-neutral-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+            Ingresar
           </button>
         </div>
       </div>
     </div>
   );
 };
+
 const ModalBGIngreso = () => {
   const [showModal, setShowModal] = useState(false);
 
@@ -134,23 +121,15 @@ const ModalBGIngreso = () => {
   };
 
   const closeModal = () => {
-    
     setShowModal(false);
   };
 
-  const handleAccept = (amount) => {
-    // Aquí puedes realizar la acción con el número ingresado
-    console.log(`Cantidad ingresada: ${amount}`);
-    
-    closeModal();
-  };
-
   return (
-    <div className="py-8 px-4 text-center">
-      <button onClick={openModal} className="bg-neutral-500 hover:bg-stone-900 hover:text-white font-bold w-full py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline mt-4 lg:mt-0">
+    <div className='flex justify-center'>
+      <button onClick={openModal} className="bg-neutral-700 hover:bg-neutral-900 text-white font-bold py-2 px-4 rounded">
         Ingresar Dinero
       </button>
-      <Modal show={showModal} handleClose={closeModal} handleAccept={handleAccept} />
+      <Modal show={showModal} handleClose={closeModal} />
     </div>
   );
 };
