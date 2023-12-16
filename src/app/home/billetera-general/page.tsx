098b98c95b3ed6f8d5fc320e939152b5403f07f0
alstreid <alexsonder01@gmail.com>
2023-12-16 00:01:49 -0500

    Implementación de fetching en billetera general

--- src/app/home/billetera-general/page.tsx
+++ src/app/home/billetera-general/page.tsx
@@ -6,27 +6,36 @@ import axios from 'axios';
 import React from 'react'
 import { BiWallet } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import { IoMdRemove } from "react-icons/io";
 import { RiSubtractFill } from "react-icons/ri";
 import { useEffect, useState } from "react"
-
+import { format } from 'date-fns';
 
 
 {/* <RiSubtractFill /> */ }
 
 export function BilleteraG() {
 
-  const [data, setData] = useState(null);
   const credentialUser = JSON.parse(sessionStorage.getItem('usuario') as string);
+  const [data, setData] = useState(null);
+  const [saldo, setAccount] = useState({});
 
-  
+
+  // Verificar si hay datos y ordenar por fecha
+  const dataORD = data && [...data].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
 
   useEffect(() => {
     // Función asíncrona para realizar la petición GET
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://127.0.0.1:3000/api/transactions/${credentialUser.cuenta}`) 
+        const response = await axios.get(`http://127.0.0.1:3000/api/transactions/${credentialUser.cuenta}`)
         setData(response.data); // Guarda los datos en el estado
-    
+
+        const account = await axios.get(`http://localhost:3000/api/users/account/${credentialUser.cuenta}`)
+        setAccount(account.data); // Guarda los datos en el estado
+
+
       } catch (error) {
         console.error('Error al obtener los datos:', error);
       }
@@ -34,13 +43,14 @@ export function BilleteraG() {
 
     // Llama a la función para realizar la petición cuando el componente se monta
     fetchData();
-  }, []); 
+  }, []);
   console.log(data)
+
 
   return (
     <div className='flex'>
 
-      <div className="w-3/5 p-4 h-[100%]">
+      <div className="w-2/3 p-4 h-[100%]">
 
         <div className="mb-6 bg-zinc-200 p-4 rounded-2xl min-h-25% shadow-xl">
           <div className='p-4'>
@@ -54,223 +64,50 @@ export function BilleteraG() {
             </div>
           </div>
         </div>
+
         <div className="mb-6 bg-zinc-200 px-4 rounded-2xl shadow-xl">
           <div><h1 className='font-bold text-xl py-5 pb-6'>Movimientos</h1></div>
-          {/* <div className="grid grid-cols-10 gap-4 py-3">
-            <div className="font-bold text-lg col-span-1"></div>
-            <div className="font-bold text-lg col-span-2">Transacción</div>
-            <div className="font-bold text-lg col-span-2">Fecha</div>
-            <div className="font-bold text-lg col-span-3">Tarjeta</div>
-            <div className="font-bold text-lg col-span-2 grid justify-items-end">Valor</div>
-
-          </div> */}
           <div className='min-h-[450px] max-h-[450px] overflow-auto scrollbar-thumb:!roudend'>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              
 
-              <div className=' flex flex-col md:flex-row items-start border-t border-gray-300' style={{
-                paddingTop: "20px",
-                marginTop: "20px"
-              }}>
-                <div className="flex-grow text-center">
-                  <p className='font-bold text-lg' >Tipo</p>
-                  <p>Debito</p>
+            {dataORD && dataORD.map((item) => (
+              <div key={item.trasac_id} className="grid grid-cols-11 gap-2 pb-7 min-h-[85px] max-h-[85px]">
+                <div className="flex items-center bg-white rounded-xl min-h-[60px] max-h-[60px] ">
+                  <div className='ml-4'>
+                    {item.ttrac_id_fk.ttrac_id === 1 ? (
+                      <IoMdAdd style={{ color: 'green' }} size={25} />
+                    ) : (
+                      <IoMdRemove style={{ color: 'red' }} size={25} />
+                    )}
+                  </div>
                 </div>
-                <div className="flex-grow text-center">
-                  <p className='font-bold text-lg'>Banco</p>
-                  <p>Pichincha</p>
-                </div>
-                <div className="flex-grow text-center">
-                  <p className='font-bold text-lg'>Fecha</p>
-                  <p>01/02/2023</p>
+                <div className="col-span-4 ">
+                  <p className='font-bold text-base' >{item.trasac_nombre}</p>
+                  <p className='text-sm'>{item.trasac_descripcion}</p>
                 </div>
 
-                <div className="flex-grow text-center">
-                  <p></p>
-                  <p id='Gastos' className="text-red-500 font-bold text-lg mt-2" >$1000.00</p>
+                <div className="px-3 col-span-2  grid text-center">
+                  <p className='font-bold text-base'>Fecha</p>
+                  <p className='text-sm'>{format(new Date(item.createdAt), 'dd/MM/yyyy')}</p>
                 </div>
 
-                <div className="flex-grow text-center">
-                  <p>Total</p>
-                  <p>$1000.00</p>
-                </div>
-              </div>
+                <div className="text-xs col-span-2 grid text-center">
+                  {item.ttrac_id_fk.ttrac_id === 1 ? (
+                    <p id='Gastos' className="text-green-500 font-bold text-base mt-2" >
+                    $ {item.trasac_cantidad}</p>
+                  ) : (
+                    <p id='Gastos' className="text-red-500 font-bold text-base mt-2" >
+                    $ {item.trasac_cantidad}</p>
+                  )}
 
+                  
+                </div>
 
-            </div>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><IoMdAdd style={{ color: 'green' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Ingreso</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
+                <div className="font-bold col-span-2 grid text-center">
+                  <p className='text-base'>Total</p>
+                  <p className='text-lg'>${item.trasac_saldo}</p>
                 </div>
               </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><IoMdAdd style={{ color: 'green' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Ingreso</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
-                </div>
-              </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-
-
-
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><IoMdAdd style={{ color: 'green' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Ingreso</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
-                </div>
-              </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><IoMdAdd style={{ color: 'green' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Ingreso</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
-                </div>
-              </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><RiSubtractFill style={{ color: 'red' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Gasto</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
-                </div>
-              </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><RiSubtractFill style={{ color: 'red' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Gasto</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
-                </div>
-              </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-            <div className="grid grid-cols-10 gap-4 pb-7">
-              <div className="flex items-center py-3 bg-white rounded-xl">
-                <div className='ml-3'><IoMdAdd style={{ color: 'green' }} size={25} />
-                </div></div>
-              <div className="col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Ingreso</p>
-                  <p className="font-bold text-xs">Tarjeta de Debito</p>
-                </div>
-              </div>
-              <div className="font-bold text-base px-3 col-span-2">
-                <div>
-                  <p className='font-bold text-base'>Lunes,</p>
-                  <p className="text-sm">22 - 12 - 2023</p>
-                </div>
-              </div>
-              <div className="font-bold text-xs col-span-3">
-                <div>
-                  <p className='font-bold text-base'>Banco Bolivariano</p>
-                  <p className="font-bold text-xs">22******15</p>
-                </div>
-              </div>
-              <div className="font-bold text-xl col-span-2 px-3 grid justify-items-end">$ 300.00</div>
-            </div>
-
-
+            ))}
 
 
           </div>
@@ -280,14 +117,14 @@ export function BilleteraG() {
       </div>
 
 
-      <div className="w-2/5 p-4 h-full">
+      <div className="w-1/3 p-4 h-full">
         <div className="mb-6 bg-neutral-100 p-2 rounded-2xl shadow-xl">
 
           <div className="flex ">
 
             <div className="flex flex-col justify-center items-center w-1/2">
               <h1 className="text-3xl font-black">Saldo Total</h1>
-              <p className="text-4xl font-semibold">$ 1,000.00</p>
+              <p className="text-4xl font-semibold">$ {saldo.cuenta_saldo}</p>
             </div>
 
 
