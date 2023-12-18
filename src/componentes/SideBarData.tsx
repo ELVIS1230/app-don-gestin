bea4bf08e9bf0e1f4e80d80217e0f6589c53d49a
alstreid <alexsonder01@gmail.com>
2023-12-17 21:34:57 -0500

    modificacion, sing out SIDEBAR

--- src/componentes/SideBarData.tsx
+++ src/componentes/SideBarData.tsx
@@ -5,8 +5,9 @@ import { FaRegUser, FaSearchDollar } from "react-icons/fa";
 import { IoCardOutline } from "react-icons/io5";
 import { BiWallet } from "react-icons/bi";
 import { TbCalendarStats, TbHome2 } from "react-icons/tb";
+import { PiSignOutDuotone } from "react-icons/pi";
 
-const SideBarData = ({ toggle }) => {
+const SideBarData = ({ toggle }:any) => {
     return (
 
         <div>
@@ -100,12 +101,12 @@ const SideBarData = ({ toggle }) => {
             } sidebar last:absolute left-4 bottom-4`} 
             >
                 <div className="mr-8 text-[1-7rem]
-                text-brown"><FaRegUser style={{ color: 'white' }} size={35}/></div>
+                text-brown"><PiSignOutDuotone style={{ color: 'white' }} size={35}/></div>
                 <div 
                 className={`${
                     toggle ? "opacity-0 delay-200" : ""
                 } text-[1rem] text-white whitespace-pre text-`}>
-                <Link href="/home/recordatorios">Perfil</Link>
+                <Link href="/">Salir</Link>
                 </div>
              </div>  
             
