90450bb36de3b5db3291aea38a1f42e23536013b
ELVIS1230 <vladimirortiz1230@gmail.com>
2024-12-07 00:21:50 -0500

    fix: Cambio de ubicación del middleware para su funcionamiento

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // Obtén el token desde las cookies
   const token = request.cookies.get("authorization");
-  console.log("ejecutandose")
-  // Si no hay token, redirige al usuario
   if (!token) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
-
-  // Permitir el acceso si hay token
   return NextResponse.next();
 }
 
-// Define las rutas en las que aplicar el middleware
 export const config = {
-  matcher: ["/home/:path*"], // Cambia "/protected/:path*" por las rutas protegidas de tu app
+  matcher: ["/home/:path*"],
 };
